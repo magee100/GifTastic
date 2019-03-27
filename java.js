3949a625f4f74ed8584ad5ab679f22fc9846501a
Magee100 <magee100@gmail.com>
2019-03-26 22:12:32 -0700

    working gif machine, no still function

--- java.js
+++ java.js
@@ -1,9 +1,13 @@
 // Giphy API 
-// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-//         cartoon + "&api_key=dc6zaTOxFJmzC&limit=5";
+
+
+
+
+
+
 
 // Variables
-var cartoons = ["Aqua Teen Hunger Force", "Bob's Burgers", "The Simpsons","American Dad", "Rugrats"];
+var cartoons = ['Aqua Teen Hunger Force',  'The Simpsons', 'American Dad', 'Rugrats'];
 console.log(cartoons);
 
 //Render Beginning Buttons
@@ -14,10 +18,53 @@ function renderButtons() {
 
     for(var i = 0; i < cartoons.length; i++) {
         var newButton = $('<button>');
+        newButton.addClass('allButtons')
+        newButton.attr('data-type',cartoons[i]);
         newButton.text(cartoons[i]);
         buttons.append(newButton);
+    
     }
 }
+
+$(document).on('click', '.allButtons', function(){
+    var type = $(this).data('type');
+    console.log(type)
+
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        type + "&api_key=Oi9wtukFofyfHIGGJBQhyNPJgKEf98HS&limit=5";
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+            .then(function(response){
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++){
+
+                    var gifDiv = $("<div>");
+
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var cartoonImage = $("<img>");
+
+                    cartoonImage.attr("src", results[i]
+                    .images.fixed_height.url);
+
+                    gifDiv.append(cartoonImage);
+                    gifDiv.append(p);
+
+                    $("#gifBox").prepend(gifDiv);
+                }
+
+            });
+});
+
+
 
 // Create new button when submit is clicked
 $("#add-cartoon").on("click", function(event) {
@@ -26,8 +73,11 @@ $("#add-cartoon").on("click", function(event) {
     var cartoon = $("#cartoon-input").val().trim();
 
    cartoons.push(cartoon);
-
+   
     renderButtons();
+     console.log(cartoons);
+     console.log('data-type'.cartoons)
+     
 });
 
 
